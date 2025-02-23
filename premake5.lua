994abeea2caa44fcc95edd2b98e066a5aafd2b91
defini7 <alexander.suhov@hotmail.com>
2025-02-23 23:25:32 +0300

    add premake5 build file

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+---@diagnostic disable: undefined-global
+project "sol2"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "On"
+    warnings "Off"
+
+    targetdir ("%{wks.location}/Build/Target/" .. OUTPUT_DIR .. "/%{prj.name}")
+    objdir ("%{wks.location}/Build/Obj/" .. OUTPUT_DIR .. "/%{prj.name}")
+
+    files { "include/sol/**" }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+
+    filter "system:macosx"
+        pic "On"
+
+    filter "system:windows"
+		systemversion "latest"
+
+        filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "speed"
